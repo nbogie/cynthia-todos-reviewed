c6452c4419eab26160ea386c47cd8528400b8e77
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-14 18:11:19 +0100

    moved to utils folder

--- src/getEnvVarOrFail.ts
+++
@@ -1,8 +0,0 @@
-export function getEnvVarOrFail(key: string) {
-  const found = process.env[key];
-  if (found) {
-    return found;
-  } else {
-    throw new Error("missing required env var:");
-  }
-}
