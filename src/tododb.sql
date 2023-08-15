872eac7823b846126bbcb59b20810e4533940c63
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-15 16:24:08 +0100

    sql queries used for todo table

--- src/tododb.sql
+++ src/tododb.sql
@@ -4,11 +4,19 @@ drop table if exists todo;
 
 create table todo(
     todo_id serial primary key,
-    task varchar(255),
+    description varchar(255),
     creation_date date not null,
     due_date date not null,
     completed boolean default 'false'
 );
 
-insert into todo (task, creation_date,due_date, completed)
+insert into todo (description, creation_date,due_date, completed)
 values ('Finish project proposal', '2023-08-15','2023-08-30',false);
+
+select * 
+from todo 
+where todo_id = 1;
+
+update todo set description = 'wash dishes', due_date = '2023-09-10', completed = false  where todo_id = 1;
+
+delete from todo where todo_id = 1;
